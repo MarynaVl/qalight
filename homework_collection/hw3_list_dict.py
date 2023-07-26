16004a61c4860ecda74ff5da653133d04561cd11
Марина Русланівна Власенко <maryna.vlasenko@infotech.gov.ua>
2023-07-26 12:41:00 +0300

    Edited

--- homework_collection/hw3_list_dict.py
+++ homework_collection/hw3_list_dict.py
@@ -6,39 +6,21 @@ dict_b: dict = {'salary': 100, 'bonus': 25, 'premium': 50}
 
 
 # Список УНІКАЛЬНИХ елементів списку (a)
-def unique_number(any_list: list) -> list:
-    unique_list = []
-    for i in any_list:
-        if i not in unique_list:
-            unique_list.append(i)
-    return unique_list
-
-
-# Список значень словника (б)
-def dict_list(any_dict: dict) -> list:
-    my_dict_list = []
-    for i in any_dict.values():
-        my_dict_list.append(i)
-    return my_dict_list
-
-
-# Перевизначити значення ключа в словнику
-def another_salary(value1: int, value2: int):
-    new_salary = value1 + value2
-    print(new_salary)
+unique_list = list(set(list_a))
 
 
 # Вивести суму усіх елементів списку (a)
 print(sum(list_a))
 
 # Вивести суму усіх УНІКАЛЬНИХ елементів списку (a)
-print(sum(unique_number(list_a)))
+print(sum(unique_list))
 
 # Надрукувати суму елемента 3 зі списку (а), та зарплати зі словника (б)
 print(int(list_a[2]) + int(dict_b['salary']))
 
 # Надрукувати повну суму доходу на основі словника (б)
-print(sum(dict_list(dict_b)))
+print(sum(dict_b.values()))
 
 # Перевизначити зарплату в словнику, збільшивши її на суму премії та надрукувати результат
-another_salary(dict_b['salary'], dict_b['premium'])
+dict_b['salary'] += dict_b['premium']
+print(dict_b['salary'])
